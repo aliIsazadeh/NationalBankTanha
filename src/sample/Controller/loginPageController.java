3b74f9461f2ad7181afb717296b753561b34573e
ghader-Salehi <amirghsl1380@gmail.com>
2020-02-20 18:10:42 -0800

    mainPage of bank sent

--- src/sample/Controller/loginPageController.java
+++ src/sample/Controller/loginPageController.java
@@ -1,16 +1,28 @@
 package sample.Controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.animation.TranslateTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
-public class loginPageController {
+public class loginPageController implements Initializable {
 
 
     public Pane movablePane;
@@ -19,6 +31,18 @@ public class loginPageController {
     public AnchorPane loginAnchorPane;
     public JFXButton signInTrans;
     public Label lblFailAlert;
+    public Label lblFailAllert;
+    public Label lblSuccsesAllert;
+    public CheckBox checkLoginPass;
+    public JFXButton btnLogin;
+    public JFXTextField txtPassLogin;
+    public JFXTextField txtUserLogin;
+    public JFXTextField txtRegisterFirstName;
+    public JFXTextField txtRegisterPassRepeat;
+    public JFXButton btnRegister;
+    public JFXTextField txtRegisterLastName;
+    public JFXTextField txtRegisterPass;
+    public CheckBox checkRegisterPass;
 
     public void signIntransPort(){
 
@@ -60,6 +84,43 @@ public class loginPageController {
 
 
     }
+    public void loadMainPage(){
+
+        Parent root;
+        try {
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            stage.close();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/FXML/mainPage.fxml"));
+            root = loader.load();
+            stage = new Stage();
+            Stage finalStage = stage;
+            finalStage.setResizable(false);
+            finalStage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void exit() {
+
+        Alert alert = new Alert(Alert.AlertType.WARNING, "آیا میخواهید خارج شوید؟ ", ButtonType.YES, ButtonType.NO);
+
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent())
+            if (result.get() == ButtonType.YES)
+                System.exit(0);
+    }
+
+
+
+
+
+
 
 
 
