059cbb614ce8e48b906c16f44f698e4801666a00
tic tac toe <shakryhsyn1@gmail.com>
2020-02-21 11:18:22 +0330

    add Specifications for Account and Bill and person class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import Extras.DBHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +19,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
