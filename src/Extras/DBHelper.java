1372e40d10f658c2758ed7977d475534bd2afeb2
tic tac toe <shakryhsyn1@gmail.com>
2020-03-07 20:40:00 +0330

    Debug DBHelper class is ended

--- src/Extras/DBHelper.java
+++ src/Extras/DBHelper.java
@@ -4,6 +4,7 @@ import DataStructure.Account;
 import DataStructure.Bill;
 import DataStructure.Person;
 import DataStructure.Transaction;
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -43,7 +44,7 @@ public class DBHelper {
             Class.forName("org.sqlite.JDBC");
             connectionForPerson = DriverManager.getConnection("jdbc:sqlite:DBPerson.db");
             statementForPerson = connectionForPerson.createStatement();
-            System.out.println("Connection Created");
+            System.out.println("Connection Created person");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -75,13 +76,14 @@ public class DBHelper {
             Class.forName("org.sqlite.JDBC");
             connectionForAccount = DriverManager.getConnection("jdbc:sqlite:DBForAccount.db");
             statementForAccount = connectionForAccount.createStatement();
+            System.out.println("connectionForAccount");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     private void creatTableForPerson() {
-        String tableSQL = "CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,accountNumber TEXT,name TEXT , lastName TEXT,nationalCode TEXT,phoneNumber TEXT,address TEXT,fatherName TEXT ,dataOFBorn TEXT,placeOfBorn TEXT , job TEXT,gender TEXT ,marriage TEXT );";
+        String tableSQL = "CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,accountNumber TEXT,firstName TEXT , lastName TEXT,nationalCode TEXT,phoneNumber TEXT,address TEXT,fatherName TEXT ,dataOFBorn TEXT,placeOfBorn TEXT , job TEXT,gender TEXT ,marriage TEXT );";
         try {
             statementForPerson.executeUpdate(tableSQL);
             System.out.println("Person table is created");
@@ -101,7 +103,7 @@ public class DBHelper {
 
 
     private void createTableForAccount() {
-        String tableSQL = "CREATE TABLE IF NOT EXISTS account (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,accountNumber TEXT , typeOfAccount TEXT , passForATM TEXT , secondPass TEXT , inventory TEXT);";
+        String tableSQL = "CREATE TABLE IF NOT EXISTS account (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,accountNumber TEXT , typeOfAccount TEXT , passForATM TEXT , secondPass TEXT , inventory TEXT , userName TEXT);";
         try {
             statementForAccount.executeUpdate(tableSQL);
         } catch (SQLException e) {
@@ -121,8 +123,9 @@ public class DBHelper {
         }
 
     }
-////// zaxire objecti az person dar dataBase
-    private void insertPerson(Person person) {
+
+    ////// zaxire objecti az person dar dataBase
+    private void insertPerson(Person person, long number) {
         connectionForPerson();
 
         String name = person.getName();
@@ -136,16 +139,16 @@ public class DBHelper {
         String job = person.getJob();
         String gender = person.getGender();
         String marriage = person.isMarriage() + "";
-        String id = person.getAccount().getAccountNumber() + "";
-        String insertSQL = "INSERT INTO person (accountNumber  ,name  , lastName,nationalCode ,phoneNumber ,address ,fatherName  ,dataOFBorn ,placeOfBorn  , job ,gender  ,marriage  ) VALUES ('" + id + "','" + name + "','" + lastName + "','" + nationalCode + "','" + phoneNumber + "','" + address + "','" + fatherName + "','" + dateOfBorn + "','" + placeOfBorn + "','" + job + "','" + gender + "','" + marriage + "');";
+        String id = number + "";
+        String insertSQL = "INSERT INTO person (accountNumber  ,firstName  , lastName,nationalCode ,phoneNumber ,address ,fatherName  ,dataOFBorn ,placeOfBorn  , job ,gender  ,marriage  ) VALUES ('" + id + "','" + name + "','" + lastName + "','" + nationalCode + "','" + phoneNumber + "','" + address + "','" + fatherName + "','" + dateOfBorn + "','" + placeOfBorn + "','" + job + "','" + gender + "','" + marriage + "');";
         ////////                                                                                                                                  id ,accountType,name ,lastName , nationalCode , phoneNumber,address,passwordFoeATM,secondPassword , inventory
         try {
             statementForPerson.executeUpdate(insertSQL);
-            System.out.println("inserted");
+            System.out.println("inserted person");
         } catch (SQLException e) {
             // TODO Auto-generated catch block
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            System.out.println(e.getMessage() + "person");
+//   //         e.printStackTrace();
         }
         closePerson();
     }
@@ -157,6 +160,7 @@ public class DBHelper {
         String condition = bill.getCondition();
         String paymentCode = bill.getPaymentCode() + "";
         String costOfBill = bill.getCostOfBill() + "";
+
         String insertSQL = "INSERT INTO bill (billingID  , paymentCode  , condition,cost ) VALUES ('" + billingID + "','" + paymentCode + "','" + condition + "','" + costOfBill + "');";
         try {
             statementForBill.executeUpdate(insertSQL);
@@ -166,21 +170,24 @@ public class DBHelper {
         }
         closeBill();
     }
-////// zaxire objecti az Account dar dataBase
+
+    ////// zaxire objecti az Account dar dataBase
     private void insertAccount(Account account) {
-        insertPerson(account.getPerson());
+
+        insertPerson(account.getPerson(), account.getAccountNumber());
         connectionForAccount();
         String accountID = account.getAccountNumber() + "";
         String accountType = account.getAccountType();
         String passForATM = account.getPasswordForATM();
         String secondPass = account.getSecondPassword();
         String inventory = account.getInventory();
-        String insertSQL = "INSERT INTO account (accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory  ) VALUES ('" + accountID + "','" + accountType + "','" + passForATM + "','" + secondPass + "','" + inventory + "');";
+        String userName = account.getUserName();
+        String insertSQL = "INSERT INTO account (accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory,userName  ) VALUES ('" + accountID + "','" + accountType + "','" + passForATM + "','" + secondPass + "','" + inventory + "','" + userName + "');";
         try {
             statementForAccount.executeUpdate(insertSQL);
-
+            System.out.println("insert account");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage() + "Account");
         }
         closeAccount();
 
@@ -206,35 +213,16 @@ public class DBHelper {
         closeTransaction();
     }
 
-////xandane tamam tarakonesh haye 1 hesab
+    ////xandane tamam tarakonesh haye 1 hesab
     private ArrayList<Transaction> readAllTransactionForPerson(long id) {
 
-//        ArrayList<Transaction> list = new ArrayList<>();
-//        try {
-//            ResultSet resultSet = statementForTransaction.executeQuery(getTransactionSQL);
-//            while (resultSet.next()) {
-//                Transaction transaction = new Transaction();
-//                Account accountFrom = new Account();
-//                accountFrom.setAccountNumber(Long.valueOf(resultSet.getString("fromAccount")));
-//                Account To = new Account();
-//                To.setAccountNumber(resultSet.getLong("ToAccount"));
-//                transaction.setTypeOfTransaction(resultSet.getString("typeOfTransaction"));
-//                transaction.setFrom(accountFrom);
-//                transaction.setTo(To);
-//                transaction.setFinished(resultSet.getBoolean("finished"));
-//                transaction.setSerialOfTransaction(resultSet.getString("serial"));
-//                transaction.setDateOfTransaction(resultSet.getString("dat"));
-//                transaction.setCostOfTransaction(resultSet.getString("cost"));
-//                list.add(transaction);
-//            }
-//        } catch (SQLException e) {
-//            System.out.println(e.getMessage());
-//        }
+
         String getTransactionSQL = "SELECT typeOfTransaction  , fromAccount  , ToAccount  , finished  , serial  , dat  , cost FROM transactions where fromAccount = '" + id + "' or toAccount = '" + id + "';";
 
         return getTransactionsFromDB(getTransactionSQL);
     }
-//bonan ishiz olmasin
+
+    //bonan ishiz olmasin
     private ArrayList<Transaction> getTransactionsFromDB(String read) {
         connectionForTransaction();
         ArrayList<Transaction> list = new ArrayList<>();
@@ -289,13 +277,14 @@ public class DBHelper {
         closeBill();
         return bill;
     }
-///xandane hesab ba estefade az shomare hesab
+
+    ///xandane hesab ba estefade az shomare hesab
     public Account readAccount(long accountNumber) {
         connectionForAccount();
         Account account = new Account();
         account.setPerson(readPerson(accountNumber));
         account.setTransactions(readAllTransactionForPerson(accountNumber));
-        String readSQL = "SELECT accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory FROM account where accountNumber = '" + accountNumber + "';";
+        String readSQL = "SELECT accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory ,userName FROM account where accountNumber = '" + accountNumber + "';";
         try {
             ResultSet resultSet = statementForAccount.executeQuery(readSQL);
             account.setAccountNumber(resultSet.getLong("accountNumber"));
@@ -303,14 +292,15 @@ public class DBHelper {
             account.setAccountType(resultSet.getString("typeOfAccount"));
             account.setSecondPassword(resultSet.getString("secondPass"));
             account.setInventory(resultSet.getString("inventory"));
-
+            account.setUserName(resultSet.getString("userName"));
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()+"4578/7");
         }
         closeAccount();
         return account;
     }
-//xandan tamam hesab haye bnk
+
+    //xandan tamam hesab haye bnk
     public ArrayList<Account> readAllAccountForPeople() {
         connectionForAccount();
         ArrayList<Account> list = new ArrayList<>();
@@ -334,18 +324,20 @@ public class DBHelper {
         return list;
     }
 
-//bonan da ishiz olmasin
+    //bonan da ishiz olmasin
     private Person readPerson(long accountNumber) {
         connectionForPerson();
         Person person = new Person();
         Account account = new Account();
-        account.setAccountNumber(accountNumber);
-        person.setAccount(account);
 
-        String readSQL = "SELECT accountNumber  ,name  , lastName,nationalCode ,phoneNumber ,address ,fatherName  ,dataOFBorn ,placeOfBorn  , job ,gender  ,marriage from person where accountNumber = '" + accountNumber + "';";
+        String readSQL = "SELECT accountNumber  ,firstName  , lastName,nationalCode ,phoneNumber ,address ,fatherName  ,dataOFBorn ,placeOfBorn  , job ,gender  ,marriage from person where accountNumber = '" + accountNumber + "';";
+
         try {
             ResultSet resultSet = statementForPerson.executeQuery(readSQL);
-            person.setName(resultSet.getString("name"));
+            account.setAccountNumber(resultSet.getLong("accountNumber"));
+            person.setAccount(account);
+
+            person.setName(resultSet.getString("firstName"));
             person.setLastName(resultSet.getString("lastName"));
             person.setAddress(resultSet.getString("address"));
             person.setNationalNumber(resultSet.getLong("nationalCode"));
@@ -357,35 +349,76 @@ public class DBHelper {
             person.setGender(resultSet.getString("gender"));
             person.setMarriage(resultSet.getBoolean("marriage"));
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()+"xxx");
         }
         closePerson();
         return person;
     }
 
 
-/// beroz kardan hesab masalan afzayesh mojodi v taghyir ramz v ..
+    /// beroz kardan hesab masalan afzayesh mojodi v taghyir ramz v ..
     public void updateAccount(Account account) {
         connectionForAccount();
         updatePerson(account.getPerson());
-        String update = " UPDATE account  set accountNumber ='" + account.getAccountNumber() + "' set typeOfAccount = '" + account.getAccountType() + "' set passForATM '" + account.getPasswordForATM() + "' set secondPass = '" + account.getSecondPassword() + "'  set inventory = '" + account.getInventory() + "';";
+        String update = " UPDATE account set accountNumber ='" + account.getAccountNumber() +"' where accountNumber ='"+account.getAccountNumber()+"';";
+        String update1 = "UPDATE account set typeOfAccount = '"+account.getAccountType()+"' where accountNumber ='"+account.getAccountNumber()+"';";
+        String update2 = "UPDATE account set passForATM = '"+account.getPasswordForATM()+"' where accountNumber ='"+account.getAccountNumber()+"';";
+        String update3 = "UPDATE account set secondPass = '"+account.getSecondPassword()+"' where accountNumber ='"+account.getAccountNumber()+"';";
+        String update4 = "UPDATE account set inventory = '"+account.getInventory()+"' where accountNumber ='"+account.getAccountNumber()+"';";
+        String update5= "UPDATE account set userName = '"+account.getUserName()+"' where accountNumber ='"+account.getAccountNumber()+"';";
+
+
+        //accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory ,userName
         try {
             statementForAccount.executeUpdate(update);
+            statementForAccount.executeUpdate(update1);
+            statementForAccount.executeUpdate(update2);
+            statementForAccount.executeUpdate(update3);
+            statementForAccount.executeUpdate(update4);
+            statementForAccount.executeUpdate(update5);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage() + "A");
         }
         closeAccount();
     }//accountNumber  , typeOfAccount,passForATM  , secondPass  , inventory
 
-// /// beroz kardan etelaat fard  masalan tagiir telephone v ..
-    public void updatePerson(Person person) {
+    // /// beroz kardan etelaat fard  masalan tagiir telephone v ..
+    private void updatePerson(Person person) {
         connectionForPerson();
 
-        String update = "UPDATE person  set accountNumber = '" + person.getAccount().getAccountNumber() + "'  set name = '" + person.getName() + "'   set lastName = '" + person.getLastName() + "' set nationalCode = '" + person.getNationalNumber() + "'  set phoneNumber = '" + person.getPhoneNumber() + "' set address = '" + person.getAddress() + "' set fatherName = '" + person.getFatherName() + "'  set dataOFBorn ='" + person.getBornTime() + "' set placeOfBorn ='" + person.getBornPlace() + "' set job = '" + person.getJob() + "' set gender ='" + person.getGender() + "'  set marriage = '" + person.isMarriage() + "' ";
+
+        String update = "UPDATE person  set accountNumber = '" + person.getAccount().getAccountNumber() + "' WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "' ;";
+        String update1 = "UPDATE person  set firstName = '"+person.getName()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update2= "UPDATE person  set lastName ='"+person.getLastName()+"';";
+        String update3 = "UPDATE person  set nationalCode ='"+person.getNationalNumber()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update4= "UPDATE person  set phoneNumber ='"+person.getPhoneNumber()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update5= "UPDATE person  set address ='"+ person.getAddress()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update6= "UPDATE person  set fatherName ='"+person.getFatherName()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update7 = "UPDATE person  set dataOFBorn ='"+person.getBornTime()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update8 = "UPDATE person  set placeOfBorn ='"+person.getBornPlace()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update9 = "UPDATE person  set job ='"+person.getJob()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+        String update10= "UPDATE person  set gender ='"+person.getGender()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "'; ";
+        String update11 = "UPDATE person  set marriage = '"+person.isMarriage()+"'WHERE accountNumber = '" + person.getAccount().getAccountNumber() + "';";
+
+
+
+        //accountNumber  ,firstName  , lastName,nationalCode ,phoneNumber ,address ,fatherName  ,dataOFBorn ,placeOfBorn  , job ,gender  ,marriage
         try {
             statementForPerson.executeUpdate(update);
+            statementForPerson.executeUpdate(update1);
+            statementForPerson.executeUpdate(update2);
+            statementForPerson.executeUpdate(update3);
+            statementForPerson.executeUpdate(update4);
+            statementForPerson.executeUpdate(update5);
+            statementForPerson.executeUpdate(update6);
+            statementForPerson.executeUpdate(update7);
+            statementForPerson.executeUpdate(update8);
+            statementForPerson.executeUpdate(update9);
+            statementForPerson.executeUpdate(update10);
+            statementForPerson.executeUpdate(update11);
+
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage() + "p");
         }
 
         closePerson();
@@ -394,17 +427,18 @@ public class DBHelper {
 
     public void updateBill(Bill bill) {
         connectionForBill();
-        String updateSQL = "UPDATE bill set billingID = '" + bill.getBillingId() + "'  set paymentCode = '" + bill.getPaymentCode() + "'  set condition = '" + bill.getCondition() + "' set cost = '" + bill.getCostOfBill() + "' ";
+
+
+        String updateSQL = "UPDATE bill set condition = '" + bill.getCondition() + "'  where  billingID = '" + bill.getBillingId() + "' and  paymentCode = '" + bill.getPaymentCode() + "';";
+
         try {
             statementForBill.executeUpdate(updateSQL);
             System.out.println("update password");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage() + "B");
         }
         closeBill();
     }
-
-
 
 
     private void closePerson() {
@@ -447,8 +481,6 @@ public class DBHelper {
         }
     }
 
-//    public static void main(String[] args) {
-//        new DBHelper();
-//    }
+
 }
 
