11cc650f27c70d96c74ee4ff7f3cbfc0b2539808
ghader-Salehi <amirghsl1380@gmail.com>
2020-03-17 17:08:20 -0700

    loginPage completed

--- src/sample/Controller/changePassWordController.java
+++ src/sample/Controller/changePassWordController.java
@@ -4,25 +4,131 @@ import DataStructure.Account;
 import Extras.DBHelper;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
-public class changePassWordController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class changePassWordController implements Initializable {
     public JFXTextField txtRecentPass;
     public JFXTextField txtNewPass;
-    public JFXButton btnSubmit;
+    public JFXButton btnChangePass;
+    public Label lblAlert;
     loginPageController loginPage = new loginPageController();
     DBHelper dbHelper = new DBHelper();
     Account account = dbHelper.readAccount(loginPage.txtUserLogin.getText());
 
-    public boolean change(){
-        boolean changed = false ;
 
-        if (txtRecentPass.getText().equals(account.getAccountPassword())){
-            account.setAccountPassword(txtNewPass.getText());
-            changed=true;
+
+    private void alert(String message, Label lbl, String color) {
+        lbl.setText(message);
+        lbl.setStyle("-fx-text-fill: " + color + ";");
+    }
+
+    public void change(){
+
+        if(txtRecentPass.getText().equals("")||txtNewPass.getText().equals("")){
+
+            alert("لطفا فیلد هارا پر کنید",lblAlert,"red");
         }
 
 
-       return changed;
+        if (txtRecentPass.getText().equals(account.getAccountPassword())){
+            account.setAccountPassword(txtNewPass.getText());
+
+            alert("رمز عبور حساب بانکی تغییر داده شد",lblAlert,"green");
+
+        }
+
+        else {
+
+            alert("رمز عبور وارد شده(رمز فعلی) اشتباه است",lblAlert,"red");
+
+
+        }
+
+
+
     }
 
+
+    public EventHandler<KeyEvent> numeric_Validation(final Integer max_Lengh) {
+        return new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                TextField txt_TextField = (TextField) e.getSource();
+                if (txt_TextField.getText().length() >= max_Lengh) {
+                    e.consume();
+                }
+                if (e.getCharacter().matches("[0-9.]")) {
+                    if (txt_TextField.getText().contains(".") && e.getCharacter().matches("[.]")) {
+                        e.consume();
+                    } else if (txt_TextField.getText().length() == 0 && e.getCharacter().matches("[.]")) {
+                        e.consume();
+                    }
+                } else {
+                    e.consume();
+                }
+            }
+        };
+    }
+    public void testPassRecent() {
+        txtRecentPass.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+
+                if (!txtNewPass.getText().equals("")) {
+
+                    btnChangePass.setDisable(false);
+
+
+                }
+
+
+            }
+        });
+
+    }
+
+
+    public void testPassNew() {
+        txtNewPass.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+
+                if (!txtRecentPass.getText().equals("")) {
+
+                    btnChangePass.setDisable(false);
+
+
+                }
+
+
+            }
+        });
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+        txtRecentPass.addEventFilter(KeyEvent.KEY_TYPED, numeric_Validation(12));
+        txtNewPass.addEventFilter(KeyEvent.KEY_TYPED, numeric_Validation(12));
+
+
+
+
+
+    }
+
+
 }
+
+
